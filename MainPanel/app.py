27d2ec1a01d9990cf3cb2ad544c1e012c3bd1d45
mrtyildiz <muratasnb@hotmail.com>
2022-05-17 11:19:32 +0300

    Update app.py

--- MainPanel/app.py
+++ MainPanel/app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template
+import requests
 
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('bootstrap_table.html', title='Bootstrap Table')
+    url = requests.get('http://127.0.0.1:5000')
+    Datas = url.json()
+    Service_Name = Datas['Service Name']
+    Status = Datas['Status']
+    length = len(Status)
+    return render_template('bootstrap_table.html', title='Service Healtcheck', Service_Name=Service_Name,Status=Status,length=length)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5020, debug=True)
